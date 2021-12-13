6342de47453d910a49409f7febb31f040c07211f
Daanii Kusnanta <danynabilgk@gmail.com>
2021-12-14 05:13:49 +0700

    refactor create map

--- js/main.js
+++ js/main.js
@@ -1,86 +1,18 @@
 import * as THREE from './three.module.js';
+import { MAP_DEFINITION } from './map.js';
 
 const UP = new THREE.Vector3(0, 0, 1);
 const LEFT = new THREE.Vector3(-1, 0, 0);
 const RIGHT = new THREE.Vector3(1, 0, 0);
 const TOP = new THREE.Vector3(0, 1, 0);
 const BOTTOM = new THREE.Vector3(0, -1, 0);
-const MOVE_SPEED = 0.8;
+const MOVE_SPEED = 1;
 const TURN_SPEED = Math.PI / 2;
 const PLAYER_RADIUS = 0.25;
 
-const mapDefinitionEasy = [
-'P #######',
-'  #   # #',
-'# # ### #',
-'#   #   #',
-'### # ###',
-'#       #',
-'### ### #',
-'#     #  ',
-'#######  '
-]
-
-const mapDefinition = [
-'P #################################################',
-'        #         #     #   # #   #       #       #',
-'# # # # # # ##### # ### # # # ### ### # # ####### #',
-'# # # # # #   #       # # #   # #     # # #   #   #',
-'### # ### # ### # ### ####### # ### # ### # ##### #',
-'#   #     # #   # #               # # #           #',
-'### ### ########### # ##### ##### # ### # #########',
-'#     # # # #     # # #   #   #       # # #     # #',
-'##### # # # # ### # ##### # ##### ##### ####### # #',
-'# # # #     # #   # #   # # #     #   # # #       #',
-'# # ########### ##### ### # ##### ### # # ### ### #',
-'#   #   # #         #   #     # #   #       # # # #',
-'### # # # # ##### ##### ##### # # ####### ### # ###',
-'# #   #       #     #       #   #   #       #     #',
-'# # # # # ######### # ### ##### # ######### ##### #',
-'# # # # # # #   #   # # # #     # # #           # #',
-'# # ##### # # ##### # # # # ##### # # # ### ### # #',
-'#   #   # # # # #   # #     #   # #   # # # #     #',
-'# ##### ### # # # # ######### ########### ##### ###',
-'#   #       #   # #     #   #     #       # #     #',
-'### ##### # ### ### ### # ### ####### # ### # ### #',
-'#         #     # # # # # #           #   # #   # #',
-'### # # # # # ### ### # # ##### # ### # ### # # ###',
-'#   # # # # # # #   #   #     # # # # #   #   # # #',
-'# # ##### ##### # ####### ####### # # # ### # ### #',
-'# # #     #   #       #         # # # #     #   # #',
-'# # ####### # ### # ### ### ##### # ### # # ### # #',
-'# #   #     # #   #   # # # #         # # # #     #',
-'# ### ### ### # ##### # # ####### # # ### # #######',
-'# # # # #   # # #   # #   #       # #   # #       #',
-'# # # # # ### ### ### # # ####### ########### #####',
-'# #     # # #       #   #   #     # # # #         #',
-'##### ### # # # # ##### ### ####### # # ##### #####',
-'#     #   #   # #       # # # #   # #   #         #',
-'# ### ### ####### # ### # ### ### # ### ##### # ###',
-'#   #   # #       # # #                       #   #',
-'# # # ### ######### # ### # # # ### # # ##### #####',
-'# # #           #     # # # # # #   # # #   #   # #',
-'# ### ##### ### # ##### # ### ##### ### # # # # # #',
-'#   #   #   #   # #     #   #   #     #   # # #   #',
-'### # # ####### ##### ############# # # ##### # ###',
-'#   # #     #   #   #     # #   #   # # #     #   #',
-'# ### ### ### ##### # # ### # ### ####### # # # ###',
-'# #     # #   #   #   #       #     #   # # # #   #',
-'##### # ##### ### ##### # ######### # # ###########',
-'# # # # #   # #     # # # #   #   #   #           #',
-'# # ### # ##### ### # # ### # ### ##### ### #######',
-'#   # #     # # #     # #   # # # #   # #   # #   #',
-'# # # # # # # ##### # # ##### # # ### ### ### ### #',
-'# #     # #         #                 #            ',
-'#################################################  '
-];
-
-const MAP_WIDTH = 2 * 25 + 1;
-const MAP_HEIGHT = 2 * 25 + 1;
-
-const createWall = function (position){
+const createWall = function (position) {
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const wall = new THREE.Mesh(geometry, material);
     wall.position.copy(position);
 
@@ -90,36 +22,50 @@ const createWall = function (position){
 }
 
 const createMap = function (scene, mapDef) {
-    let map = {};
-    map.top = 0;
-    map.bottom = -(mapDef.length - 1);
-    map.left = 0;
-    map.right = mapDef[0].length - 1;
-    map.playerSpawn = null;
-    
-    for(let i = 0; i < mapDef.length; i++){
-        let y = -i; 
-        map[y] =  {};    
-        for(let j = 0; j < mapDef[0].length; j++){
+    const mapString = mapDef.map;
+    const mapHeight = mapDef.map.length;
+    const mapWidth = mapDef.map[0].length;
+
+    const map = {
+        top: 0,
+        bottom: mapHeight - 1,
+        left: 0,
+        right: mapWidth - 1,
+        playerSpawn: new THREE.Vector3(mapDef.start[0], -mapDef.start[1], 0),
+        goal: new THREE.Vector3(mapDef.goal[0], -mapDef.goal[1], 0),
+    };
+
+    for (let i = 0; i < mapHeight; i++) {
+        let y = -i;
+        map[y] = {};
+        for (let j = 0; j < mapWidth; j++) {
             let x = j;
-            let mapNow = mapDef[i][j];
-            
+            let mapNow = mapString[i][j];
+
             let mesh = null;
 
-            if(mapNow == 'P'){
-                map.playerSpawn = new THREE.Vector3(x,y,0);
+            if (map.playerSpawn.equals(new THREE.Vector3(x, y, 0))) {
                 map[y][x] = {
                     'isPassable': true
                 };
-            }else if(mapNow == '#'){
-                mesh = createWall(new THREE.Vector3(x,y,0))
-            }else{
+            } else if (map.goal.equals(new THREE.Vector3(x, y, 0))) {
+                const geometry = new THREE.SphereGeometry(PLAYER_RADIUS / 2);
+                const material = new THREE.MeshBasicMaterial({ color: 0xFFD700 });
+                mesh = new THREE.Mesh(geometry, material);
+
+                mesh.position.copy(map.goal);
+                mesh.isPassable = true;
+            }
+
+            if (mapNow == '#') {
+                mesh = createWall(new THREE.Vector3(x, y, 0))
+            } else {
                 map[y][x] = {
                     'isPassable': true
                 };
             }
 
-            if(mesh != null){
+            if (mesh != null) {
                 map[y][x] = mesh;
                 scene.add(mesh);
             }
@@ -159,7 +105,7 @@ const createPlayer = function (scene, position) {
     scene.add(player);
 
     return player;
-    
+
 }
 
 const createRenderer = function () {
@@ -176,7 +122,7 @@ const createScene = function () {
 
     // Add Ambient lighting
     scene.add(new THREE.AmbientLight(0x888888));
-    
+
     return scene;
 };
 
@@ -202,7 +148,7 @@ let createKeys = function () {
     return keys;
 };
 
-const animationLoop = function(callback){
+const animationLoop = function (callback) {
     let previousFrameTime = window.performance.now();
 
     let animationSeconds = 0;
@@ -212,7 +158,7 @@ const animationLoop = function(callback){
         var animationDelta = (now - previousFrameTime) / 1000;
         previousFrameTime = now;
 
-        animationDelta = Math.min(animationDelta, 1/30);
+        animationDelta = Math.min(animationDelta, 1 / 30);
 
         animationSeconds += animationDelta;
 
@@ -228,7 +174,7 @@ const checkPassable = function (map, position) {
     let x = Math.round(position.x);
     let y = Math.round(position.y);
 
-    if(map[y] && map[y][x]){
+    if (map[y] && map[y][x]) {
         return map[y][x].isPassable;
     }
     return false;
@@ -239,7 +185,9 @@ const main = function () {
     const renderer = createRenderer();
     const scene = createScene();
 
-    const map = createMap(scene, mapDefinitionEasy);
+    const mapDefinition = MAP_DEFINITION['easy'];
+
+    const map = createMap(scene, mapDefinition);
     console.log(map)
     const player = createPlayer(scene, map.playerSpawn);
 
@@ -250,13 +198,13 @@ const main = function () {
     // scene.add(cube)
 
     const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
-    
+
     camera.up.copy(UP);
     camera.targetPosition = new THREE.Vector3();
     camera.targetLookAt = new THREE.Vector3();
     camera.lookAtPosition = new THREE.Vector3();
 
-    const movePlayer = function(delta){        
+    const movePlayer = function (delta) {
         // Move based on current keys being pressed.
         if (keys['W']) {
             // W - move forward
@@ -270,11 +218,11 @@ const main = function () {
             player.translateOnAxis(LEFT, -MOVE_SPEED * delta);
             player.distanceMoved += MOVE_SPEED * delta;
         }
-        if (keys['A']){
+        if (keys['A']) {
             player.direction.applyAxisAngle(UP, TURN_SPEED * delta);
-            
+
         }
-        if (keys['D']){
+        if (keys['D']) {
             player.direction.applyAxisAngle(UP, -TURN_SPEED / 2 * delta);
         }
 
@@ -298,13 +246,13 @@ const main = function () {
 
     }
 
-    const update = function(delta){
+    const update = function (delta) {
         updateCamera(delta);
         updatePlayer(delta);
     }
 
     let _lookAt = new THREE.Vector3();
-    const updatePlayer = function(delta){
+    const updatePlayer = function (delta) {
         player.up.copy(player.direction).applyAxisAngle(UP, -Math.PI / 2);
         player.lookAt(_lookAt.copy(player.position).add(UP));
 
@@ -325,7 +273,7 @@ const main = function () {
         camera.lookAt(camera.lookAtPosition);
     }
 
-    
+
     animationLoop(function (delta) {
         update(delta);
 
