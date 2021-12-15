af6555e74f3b0f4a92a3a1e1097735197021f0f9
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-12-15 14:52:27 +0700

    Create mapHelper.js

---
+++ js/mapHelper.js
@@ -0,0 +1,59 @@
+import * as THREE from './three.module.js';
+
+/**
+ * 
+ * @param {Array.<THREE.Mesh[]>} map 
+ * @param {THREE.Vector3} position 
+ * @returns {THREE.Mesh}
+ */
+export const getAt = (map, position) => {
+    const x = Math.round(position.x);
+    const y = Math.round(position.y);
+
+    if (map[y] && map[y][x]) {
+        return map[y][x];
+    }
+
+    return null;
+}
+
+/**
+ * 
+ * @param {Array.<THREE.Mesh[]>} map 
+ * @param {THREE.Vector3} position 
+ * @returns {boolean}
+ */
+export const checkPassable = function (map, position) {
+    const mesh = getAt(map, position);
+
+    if (mesh) {
+        return mesh.isPassable;
+    }
+    return false;
+}
+
+/**
+ * 
+ * @param {Array.<THREE.Mesh[]>} map 
+ * @param {THREE.Vector3} position 
+ * @param {Number} playerRadius
+ * @returns {boolean}
+ */
+export const checkGoal = (map, position, playerRadius) => position.distanceToSquared(map.goal) < playerRadius * playerRadius / 4;
+
+/**
+ * 
+ * @param {Array.<THREE.Mesh[]>} map 
+ * @param {THREE.Vector3} position 
+ * @param {Number} playerRadius
+ * @returns {boolean}
+ */
+export const checkPowerUp = function (map, position, playerRadius) {
+    const mesh = getAt(map, position);
+
+    if (mesh && mesh.isPowerUp) {
+        return position.distanceToSquared(mesh.position) < playerRadius * playerRadius / 4;
+    }
+
+    return false;
+}
